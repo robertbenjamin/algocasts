17c0240d4cb88d7ffe034dc1e4cdc7eede524ad3
Robert Benjamin <rotberj@gmail.com>
2018-07-16 23:31:01 -0700

    Solve fizzbuzz exercise

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,32 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+const fizzBuzz = n => {
+  for (let i = 1; i <= n; i++) {
+    let char = '';
+    if (i % 3 === 0) char += 'fizz';
+    if (i % 5 === 0) char += 'buzz';
+    console.log(char || i);
+  }
+}
 
 module.exports = fizzBuzz;
+
+// --- Alternate Solutions
+
+// More traditional solution
+/*const fizzBuzz = n => {
+  for (let i = 1; i <= n; i++) {
+    const divisibleByThree = i % 3 === 0;
+    const divisibleByFive = i % 5 === 0;
+    if (divisibleByThree && divisibleByFive) {
+      console.log('fizzbuzz');
+    } else if (divisibleByThree) {
+      console.log('fizz');
+    } else if (divisibleByFive) {
+      console.log('buzz');
+    } else {
+      console.log(i)
+    }
+  }
+}*/
