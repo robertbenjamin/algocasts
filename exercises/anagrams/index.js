0b24997b6a1dcf0fbd4cc395cb889c23b528eb2c
Robert Benjamin <rotberj@gmail.com>
2018-07-20 21:14:27 -0700

    Solve anagrams exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,49 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const anagrams = (stringA, stringB) => {
+  const cleanString = string => string
+    .replace(/[^0-9a-z]/gi, '')
+    .toLowerCase();
+
+  const buildCharMap = string => {
+    const charMap = {};
+    string.split('').forEach(char => {
+      charMap[char] = charMap[char] + 1 || 1;
+    });
+    return charMap;
+  }
+
+  const cleanStringA = cleanString(stringA);
+  const cleanStringB = cleanString(stringB);
+  const charMapA = buildCharMap(cleanStringA);
+  const charMapB = buildCharMap(cleanStringB);
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+
+  for (let key in charMapA) {
+    if (charMapA[key] !== charMapB[key]) return false;
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
+
+// --- Alternate Solutions
+
+// My version of solution from course
+/*const anagrams = (stringA, stringB) => {
+  const sortedString = string => {
+    return string
+      .toLowerCase()
+      .replace(/\W/g, '')
+      .split('')
+      .sort()
+      .join();
+  }
+
+  return sortedString(stringA) === sortedString(stringB);
+}*/
