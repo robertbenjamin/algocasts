b8e2619a869fbfa21cb3bc84fc6812878381a320
Robert Benjamin <rotberj@gmail.com>
2018-07-23 23:48:59 -0700

    Solve steps exercise

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,71 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+const steps = (n, row = 1, stairs = []) => {
+  if (n === 0) return;
+
+  for (let i = 1; i <= n - 1; i++) {
+    stairs.unshift(' ');
+  }
+
+  for (let i = 1; i <= row; i++) {
+    stairs.unshift('#');
+  }
+
+  console.log(stairs.join(''));
+  return steps(n - 1, row + 1);
+}
 
 module.exports = steps;
+
+// --- Alternate Solutions
+
+// My first solution
+/*const stepString = (stepLength, totalLength) => {
+  let step = [];
+
+  for (let i = 0; i < stepLength; i++) {
+    step.push('#');
+  }
+  for (let i = 0; i < totalLength - stepLength; i++) {
+    step.push(' ');
+  }
+
+  return step.join('');
+}
+
+const steps = n => {
+  for (let i = 1; i <= n; i++) {
+    console.log(stepString(i, n));
+  }
+}*/
+
+// Given solution
+/*const steps = n => {
+  for (let row = 0; row < n; row++) {
+    let string = '';
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        string += '#';
+      } else {
+        string += ' ';
+      }
+    }
+
+    console.log(string);
+  }
+}*/
+
+// Given recursive solution
+/*const steps = (n, row = 0, stair = '') => {
+  if (n === row) return;
+
+  if (stair.length === n) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? '#' : ' ';
+  return steps(n, row, stair + add);
+}*/
