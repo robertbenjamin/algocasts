82702c53e6d6da918b180c4721a801303864f44a
Robert Benjamin <rotberj@gmail.com>
2018-07-25 22:38:32 -0700

    Solve pyramid exercise

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,64 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+const pyramid = n => {
+  for (let row = 1; row <= n; row++) {
+    let level = '';
+
+    const spaces = n - row;
+    const hashes = row * 2 - 1;
+
+    for (let count = 1; count <= spaces; count++) level += ' ';
+    for (let count = 1; count <= hashes; count++) level += '#';
+    for (let count = 1; count <= spaces; count++) level += ' ';
+
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
+
+// --- Alternate Solutions
+
+// My second solution
+/*const pyramid = n => {
+  const width = n * 2 - 1;
+  const mid = Math.floor(width / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = '';
+
+    for (let column = 0; column < width; column++) {
+      const before = column < mid - row;
+      const after = column > mid + row;
+      level += (before || after) ? ' ' : '#';
+    }
+
+    console.log(level);
+  }
+}*/
+
+// My recursive solution
+/*const pyramid = (n, row = 0, level = '') => {
+  if (n === row) return;
+
+  const width = n * 2 - 1;
+  const mid = Math.floor(width / 2);
+
+  if (level.length === width) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const index = level.length;
+  const before = index < mid - row;
+  const after = index > mid + row;
+  level += (before || after) ? ' ' : '#';
+  return pyramid(n, row, level);
+}*/
