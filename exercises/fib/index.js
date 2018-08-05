f89852824342c20a25ecb330e7311e62afe04150
Robert Benjamin <rotberj@gmail.com>
2018-08-05 16:21:16 -0700

    Solve fibonacci exercise

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,62 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+const fib = (n, i = 0, curr = 0, next = 1) => {
+  if (i === n) return curr;
+  return fib(n, ++i, next, next + curr);
+}
 
 module.exports = fib;
+
+// --- Alternate Solutions
+
+/*const fib = n => {
+  let curr = 0;
+  let oldCurr;
+  let next = 1;
+
+  for (let i = 0; i < n; i++) {
+    oldCurr = curr;
+    curr = next;
+    next = oldCurr + next;
+  }
+
+  return curr;
+}*/
+
+// Given solution
+/*const fib = n => {
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1];
+    const b = result[i - 2];
+
+    result.push(a + b);
+  }
+
+  return result[n];
+}*/
+
+// Given recursive solution
+/*const fib = n => {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+}*/
+
+// Given memoized recursive solution
+/*const memoize = fn => {
+  const cache = {};
+
+  return (...args) => {
+    if (cache[args]) return cache[args];
+    return cache[args] = fn(...args);
+  }
+}
+
+let fib = n => {
+  if (n < 2) return n;
+  return fib(n - 2) + fib (n - 1);
+}
+
+fib = memoize(fib);*/
