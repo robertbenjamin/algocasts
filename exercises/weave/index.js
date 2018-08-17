23a8927a5b8134e487ee7f7631dbefdf78a9ac6c
Robert Benjamin <rotberj@gmail.com>
2018-08-17 15:25:38 -0700

    Solve weave exercise

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,15 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+const weave = (sourceOne, sourceTwo) => {
+  const weavedQueue = new Queue();
+
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) weavedQueue.add(sourceOne.remove());
+    if (sourceTwo.peek()) weavedQueue.add(sourceTwo.remove());
+  }
+
+  return weavedQueue;
+}
 
 module.exports = weave;
