d2a795b9ec43268f7e5706d3779ae51ffd5c8656
Robert Benjamin <rotberj@gmail.com>
2018-09-30 23:53:54 -0700

    Add removeLast method to linkedlist - Remove unnecessary counter from getLast method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -35,17 +35,15 @@ class LinkedList {
   }
 
   getLast() {
-    let counter = 0;
     let node = this.head;
-    let prevNode = null;
+    let previous = null;
 
     while(node) {
-      counter++;
-      prevNode = node;
+      previous = node;
       node = node.next;
     }
 
-    return prevNode;
+    return previous;
   }
 
   clear() {
@@ -55,6 +53,24 @@ class LinkedList {
   removeFirst() {
     if (!this.head) return;
     this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) return;
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+
+    previous.next = null;
   }
 }
 
