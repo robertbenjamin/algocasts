004e173807ec4348dc9df6da2f8358d67c2d70bf
Robert Benjamin <rotberj@gmail.com>
2018-10-14 18:31:46 -0700

    Add iterator to linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -129,6 +129,14 @@ class LinkedList {
       index++;
     }
   }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
